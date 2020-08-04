76f34002cc5af8dbe6e98e6b37f0131cfb8fd4bf
CarolGalvao <56117802+CarolGalvao@users.noreply.github.com>
2020-08-03 23:25:17 -0300

    init github

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Landing from './pages/Landing'
+
+//Nome do componente sempre começa com letra maiuscula
+//JSD = javaScript + XML
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
